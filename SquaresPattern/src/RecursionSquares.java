ee118309cd7e77b6bef621db234f579a0af016df
kafumji <kafumji@LAPTOP-8P8F41K0.attlocal.net>
2018-06-02 22:21:52 -0700

    Added ability for user to set starting size

--- SquaresPattern/src/RecursionSquares.java
+++ SquaresPattern/src/RecursionSquares.java
@@ -1,19 +1,23 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 /**
    @author Evan Ugarte
    @version 1.0
 */
 
-public class RecursionSquares extends JPanel {
+public class RecursionSquares extends JPanel{
 
-private final static int WIDTH = 500, HEIGHT = 500, START_SIZE = 200, MIN_SIZE = 15;
+	private final static int WIDTH = 500, HEIGHT = 500;
+	private int MIN_SIZE = 2, START_SIZE = 250;
+
 	
-	public RecursionSquares() {
+	public RecursionSquares(int max, int min) {
 		super();
 		setBackground(Color.white);
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
-
+		START_SIZE = max;
 	}
 	
 	public void paintComponent(Graphics pen) {
@@ -40,13 +44,52 @@ private final static int WIDTH = 500, HEIGHT = 500, START_SIZE = 200, MIN_SIZE =
 	
 	
 	public static void main(String[] args) {
-		RecursionSquares panel = new RecursionSquares();
-	
 		JFrame frame = new JFrame("Recursive Squares!");
-		frame.getContentPane().add(panel);
+		JLabel startLbl = new JLabel();
+		startLbl.setText("Indicate Shape Start Size:");
+		JLabel endLbl = new JLabel();
+		endLbl.setText("Indicate Smallest Shape Size:");
+		JLabel errLbl = new JLabel();
+		errLbl.setText("Please Enter A Positive Numeric Value");
+		JTextField startSize = new JTextField(20);
+		JButton startBtn = new JButton("Start");
+		
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(startLbl);
+		//frame.getContentPane().add(endLbl);
+		frame.getContentPane().add(startSize);
+		frame.getContentPane().add(startBtn);
+		frame.getContentPane().add(errLbl);
+		errLbl.setVisible(false);
+		startBtn.addActionListener(new ActionListener()
+		{
+			  public void actionPerformed(ActionEvent e)
+			  {
+				  String startVal = startSize.getText();
+				  if(isInt(startVal)){
+					  RecursionSquares panel = new RecursionSquares(Integer.parseInt(startVal), 
+							  2);
+					  frame.setContentPane(panel);
+					  frame.invalidate();
+					  frame.validate();
+				  }else {
+					  errLbl.setVisible(true);
+				  }
+			  }
+			});
+		frame.pack();
 		frame.setSize(500,500);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static boolean isInt(String s) {
+		try {
+			Integer.parseInt(s);
+			return true;
+		}catch(Exception e) {
+			return false;
+		}
 	}
 	
 }
